ff490c67450562abe1177a6d0e9abba78e955504
S. Aria Hosseini <aria.hosseini73@gmail.com>
2020-01-31 18:01:58 -0800

    Add files via upload

--- ITC_mismatchmodels.py
+++ ITC_mismatchmodels.py
@@ -158,17 +158,19 @@ def gaussian_distribution(sigma, expected_value, num_qpoints, lattice_parameter)
 
 def single_wave(path_to_mass_weighted_hessian, path_to_atoms_positions, num_atoms,
                 num_atoms_unit_cell, central_unit_cell, lattice_parameter, intersection, frq_mode,
-                idx_ko, sigma, expected_value, num_cell,
-                origin_unit_cell=1, skip_lines=16, num_qpoints=1000):
-    positions = atoms_position(path_to_atoms_positions, num_atoms, num_atoms_unit_cell, origin_unit_cell, skip_lines)
-    frq = acoustic_phonon(path_to_mass_weighted_hessian, path_to_atoms_positions, num_atoms, num_atoms_unit_cell,
-                          central_unit_cell, lattice_parameter, intersection, skip_lines, num_qpoints)
-    gaussian = gaussian_distribution(sigma, expected_value, num_qpoints, lattice_parameter)
+                idx_ko, sigma, expected_value, num_cell, rep,
+                origin_unit_cell=1, skip_lines=[16, 9], num_qpoints=1000):
+    positions = atoms_position(path_to_atoms_positions[1], num_atoms[1], num_atoms_unit_cell, origin_unit_cell,
+                               skip_lines[1])
+    solid_I_positions = positions[1][:num_cell*rep[0]*rep[1]]
+    frq = acoustic_phonon(path_to_mass_weighted_hessian, path_to_atoms_positions[0], num_atoms[0], num_atoms_unit_cell,
+                          central_unit_cell, lattice_parameter, intersection, skip_lines[0], num_qpoints)
+    gaussian = gaussian_distribution(sigma, expected_value, num_qpoints, lattice_parameter)[frq_mode]
     points = qpoints(num_qpoints, lattice_parameter)
     sign_correction = np.exp(-1j * (np.arctan(frq[2][frq_mode, idx_ko].imag / frq[2][frq_mode, idx_ko].real)))
-    wave = gaussian[idx_ko]*np.multiply(numpy.matlib.repmat(frq[frq_mode], num_cell, 1),
-                                        -1j*np.exp(positions[1]*points[idx_ko])*sign_correction)
-    return wave
+    wave = gaussian[idx_ko]*np.multiply(numpy.matlib.repmat(frq[frq_mode::3], num_cell, 1),
+                                         -1j*np.exp(np.matmul(solid_I_positions, points[:, idx_ko][np.newaxis].T))*sign_correction)
+    return positions, frq, gaussian, sign_correction, points
 
 
     # u = zeros(size(atom_site, 1), 3);
@@ -260,7 +262,7 @@ class ITC:
     # def equilibrium_thermal_conductance(self):
 
 
-A = ITC(rho=[1.2, 1.2], c=[2, 1])
+# A = ITC(rho=[1.2, 1.2], c=[2, 1])
 # B = A.acoustic_mismatch()
 # plt.polar(np.arccos(B[0]), B[-2], 'o')
 # plt.show()
@@ -287,15 +289,15 @@ A = ITC(rho=[1.2, 1.2], c=[2, 1])
 # plt.plot(vDoS[1])
 # plt.show()
 
-B = atoms_position('~/Desktop/cleanUpDesktop/LamResearch_Internship_Summer_2019/'
-                   'Run-14-hessian-analysis/Run-05-Si/data.Si-5x5x5', 1000, 8, 63, skip_lines=16)
+# B = atoms_position('~/Desktop/cleanUpDesktop/LamResearch_Internship_Summer_2019/'
+#                    'Run-14-hessian-analysis/Run-05-Si/data.Si-5x5x5', 1000, 8, 63, skip_lines=16)
 
 
-C = single_wave("~/Desktop/cleanUpDesktop/LamResearch_Internship_Summer_2019/"
-                "Run-14-hessian-analysis/Run-06-Ge/Si-hessian-mass-weighted-hessian.d",
-                "~/Desktop/cleanUpDesktop/LamResearch_Internship_Summer_2019/"
-                "Run-14-hessian-analysis/Run-05-Si/data.Si-5x5x5", 1000, 8, 63, 5.43, 901, 2, 200, 0.001, 0.005, 10,
-                origin_unit_cell=1, skip_lines=16, num_qpoints=1000)
+C = single_wave("~/Desktop/ITC/Run-00-si-heavy-si/Run-01-hessian/Si-hessian-mass-weighted-hessian.d",
+                ["~/Desktop/ITC/Run-00-si-heavy-si/Run-01-hessian/data.Si-5x5x5",
+                 "~/Desktop/ITC/Run-00-si-heavy-si/Run-00-configuration-add-heavy-si/data.unwraped"],
+                [1000, 5*5*400+5*5*400], 8, 63, 5.43, 901, 2, 400, 0.01, 0.005, 10, [5,5,400],
+                origin_unit_cell=1, num_qpoints=1000)
 
 
 
