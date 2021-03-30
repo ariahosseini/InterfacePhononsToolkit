dac226222a4d85027bf192711c06b876f19ef101
S. Aria Hosseini <aria.hosseini73@gmail.com>
2021-03-30 01:41:07 -0700

    Add files via upload

---
+++ util/autocorrelation_func.py
@@ -0,0 +1,38 @@
+# Define and test the ACF function
+
+def ACF(t, J):
+    # Compute the autocorrelation of the instantaniouse flux vector
+    time_intervals = t.size
+    c = np.array([[np.dot(J[j, :time_intervals - i], J[j, i:]) for i in range(time_intervals)] for j in range(3)])
+    c *= (t[-1] - t[0]) / float(time_intervals)
+    return c
+
+
+def ACF_FFT(t_max, J):
+    # Compute the autocorrelation of the instantaniouse flux vector
+    nd = J.shape()
+    time_intervals = nd[1]
+    c = np.zeros([3, time_intervals * 2])
+    zpad = np.zeros(time_intervals)
+    sf = t_max / float(time_intervals)
+    for j in range(3):
+        dft = np.fft.fft(np.concatinate(J[j], zpad))
+        c[j] = np.fft.ifft(dft * np.conjugate(dft)) * sf
+    return c[:, :time_intervals]
+
+
+def ACF_NP(t, J):
+    # Compute the autocorrelation of the instantaniouse flux vector
+    c = [np.correlate(J[i]) for i in range(3)]
+    return c
+
+
+def ACF2(t, J):
+    # Compute the autocorrelation of the instantaniouse flux vector. Same opperation as above, but laid out differently
+    c = 0 * J
+    time_intervals = t.size
+    for j in range(3):
+        for i in range(time_intervals):
+            c[j, i] = np.dot(J[j, :time_intervals - i], J[j, i:])
+    c *= (t[-1] - t[0]) / float(time_intervals)
+    return c
