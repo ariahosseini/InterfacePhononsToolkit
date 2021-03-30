f7bf2bed9e81cc4b4a72d5b65d90d86a5ceec215
S. Aria Hosseini <aria.hosseini73@gmail.com>
2021-03-30 01:39:25 -0700

    Add files via upload

---
+++ util/generate_figs.py
@@ -0,0 +1,42 @@
+# Define plotting functions
+
+plt.rc("font", size=18, family='sans-serif')
+plt.rcParams["figure.figsize"] = (6, 5)
+plt.rcParams['animation.html'] = 'html5'
+
+
+def quickplot(x, y, xlab="x", ylab="y", plotlab=""):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(x, y, '-')
+    ax.set(title=plotlab, xlabel=xlab, ylabel=ylab)
+    plt.show()
+
+
+def quickploterr(x, y, e, col='', xlab="x", ylab="y", plotlab=""):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(x, y, '-' + col)
+    ax.fill_between(x, y - e, y + e, alpha=0.2)
+    ax.set(title=plotlab, xlabel=xlab, ylabel=ylab)
+    # plt.savefig('test.pdf')
+    plt.show()
+
+
+def quickmultiplot(data, xlab="x", ylab="y", plotlab=""):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for x, y in data:
+        ax.plot(x, y, '-')
+        ax.set(title=plotlab, xlabel=xlab, ylabel=ylab)
+    plt.show()
+
+
+def quickmultierrplot(data, xlab="x", ylab="y", plotlab=""):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for x, y, e in data:
+        ax.plot(x, y, '-')
+        ax.fill_between(x, y - e, y + e, alpha=0.2)
+        ax.set(title=plotlab, xlabel=xlab, ylabel=ylab)
+    plt.show()
