4c62a272acf25748cc8090d9987936b7be651a5d
S. Aria Hosseini <shoss008@ucr.edu>
2021-04-01 12:29:43 -0700

    Update effective_medium.py

--- util/effective_medium.py
+++ util/effective_medium.py
@@ -1,11 +1,9 @@
 import numpy as np
-import math
 from scipy.optimize import curve_fit
-import os
 
 
-def Logistic(mfp, lc):
-    return S_diffusive / (1 + mfp / lc)
+def Logistic(mfp, lambda_o):
+    return kappa_bulk / (1 + lambda_o / mfp)
 
 
 def Minnich(phi, Lp):
@@ -40,47 +38,14 @@ def Rayleigh(phi):
 def bulk_mean_free_path(path_kappa_cumulative, phi, So, maxfev=1000):
 
     cumulative_data = np.loadtxt(path_kappa_cumulative, skiprows=1, delimiter=',')
-
-    lambda_bulk = np.array([])
-
-    for porosity_index in range(np.shape(phi)[1]):
-        global S_diffusive
-        S_diffusive = So[0, porosity_index]
-        _lambda_bulk, _ = curve_fit(Logistic, cumulative_data[:, 0], cumulative_data[:, 1], maxfev=maxfev)
-        lambda_bulk = np.append(lambda_bulk, _lambda_bulk)
-        del S_diffusive
+    lambda_bulk, _ = curve_fit(Logistic, cumulative_data[:, 0], cumulative_data[:, 1], maxfev=maxfev)
 
     return lambda_bulk
 
+def kappa_effective(So, Lc, Lambda_o):
 
-path_kappa_cumulative = '../kappa_almabte/GaN_30_30_30_1,0,0_800K.kappacumul_MFP'
-# cumulative_data = np.loadtxt(path_kappa_cumulative, skiprows=1, delimiter=',')
-# maxfev = 1000
+    Kn = Lambda_o / Lc
+    zeta = (1 + Kn * (np.log(Kn) - 1)) / (Kn - 1)**2
+    kappa_effective = kappa_bulk * So.T * zeta
 
-# lambda_bulk, a = curve_fit(Logistic, cumulative_data[:, 0], cumulative_data[:, 1], maxfev=maxfev)
-
-# print(lambda_bulk)
-
-phi = np.array([[0.05, 0.25, 0.40, 0.55, 0.70, 0.15]])
-Lp = np.array([[40, 80, 100, 500, 1000, 4000, 10000]])
-
-So = Rayleigh(phi=phi)
-Lc = Liu(phi=phi, Lp=Lp)
-# print(np.shape(Lc))
-
-# lambda_bulk = np.array([])
-
-# for porosity_index in range(np.shape(phi)[1]):
-
-#     global S_diffusive
-#     S_diffusive = So[0, porosity_index]
-#     _lambda_bulk, _ = curve_fit(Logistic, cumulative_data[:, 0], cumulative_data[:, 1], maxfev=maxfev)
-#     lambda_bulk = np.append(lambda_bulk, _lambda_bulk)
-#     del S_diffusive
-
-
-Lambda_o = bulk_mean_free_path(path_kappa_cumulative=path_kappa_cumulative, phi=phi, So=So, maxfev=1000)
-print(Lambda_o)
-
-
-exit()
+    return Kn, zeta, kappa_effective
