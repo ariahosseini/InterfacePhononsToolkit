40ed6f8822c08120e90baed8fd01b63a0ed27d3f
S. Aria Hosseini <shoss008@ucr.edu>
2021-04-01 20:33:12 -0700

    Update autocorrelation_func.py

--- util/autocorrelation_func.py
+++ util/autocorrelation_func.py
@@ -1,5 +1,5 @@
 """
-autocorrelation_func.py compute the autocorrelation (ACF) and fast fourier transfer of ACF
+autocorrelation_func.py compute the autocorrelation (ACF) using defferent methods
 
 Author: S. Aria Hosseini
 Email: shoss008@ucr.edu
@@ -28,7 +28,9 @@ def ACF(time, J):
 
 
 def ACF_FFT(t_max, J):
+
     # Compute the fast fourier transform of the instantaneous flux vector autocorrelation
+
     nd = np.shape(J)
     print(nd)
     time_intervals = nd[1]
@@ -40,3 +42,34 @@ def ACF_FFT(t_max, J):
         dft = np.fft.fft(np.concatenate([J[j], zpad]))
         c[j] = np.fft.ifft(dft * np.conjugate(dft)) * sf
     return c[:, :time_intervals]
+
+
+def ACF_NP(t, J):
+
+    # Compute the autocorrelation of the instantaneous flux vector using numpy correlation command
+
+    acf = [np.correlate(J[i]) for i in range(3)]
+    return acf   # Autocorrelation
+
+
+def ACF2(time, J):
+
+    """
+
+    Compute the autocorrelation of the instantaneous heat flux vector.
+    Same opperation as ACF, but laid out differently
+
+    :arg
+        time                                 : Autocorrelation time lag
+        J                                    : Instantaneous heat flux vector, Numpy array of 3 by np.shape(time)
+    :returns
+        acf                                  : Autocorrelation
+    """
+    
+    acf = 0 * J
+    time_intervals = time.size
+    for j in range(3):
+        for i in range(time_intervals):
+            acf[j, i] = np.dot(J[j, :time_intervals - i], J[j, i:])
+        acf *= (time[-1]-time[0])/float(time_intervals)
+    return acf
