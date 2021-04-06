9f30f73d29c1c0627a4deb4dd3c56a0335bb8926
S. Aria Hosseini <shoss008@ucr.edu>
2021-04-06 11:01:17 -0700

    Update RAY_TRACING.py

--- examples/Ex2-Ray-Tracing/RAY_TRACING.py
+++ examples/Ex2-Ray-Tracing/RAY_TRACING.py
@@ -1,22 +1,38 @@
-import numpy as np
-import time
-import os
-# get current directory
-from util.autocorrelation_func import *
+"""
+
+A Example of running simulation to model anticorrelation effect in materials containing nanoscale porosity
+
+"""
+
+# Import ray_trying method, the required libraries are automatically imported
 from util.generate_figs import *
 from util.ray_tracing import *
-
-# path = os.getcwd()
-# print("Current Directory", path)
-# prints parent directory
-# print(os.path.abspath(os.path.join(path, os.pardir)))
+import time
 
 
-
-# Function to write a block of data as a big file
+# Define a function to write a block of data as a big file
 def WriteDataFile(outfile, v_g, tau_mean, Lp, tau_max, alphas, data, uncertainty, time_intervals):
+    """
+    Generate the output file
 
-    paramkey = 'Parameters: v_g='+str(v_g)+' tau_mean='+str(tau_mean)+' Lp='+str(Lp)+' tau_max='+str(tau_max)+' Kn='+str(v_g*tau_mean/Lp)+'\n'
+    :arg
+        outfile                                 : Output file's name
+        time_intervals                          : Time intervals
+        v_g                                     : Phonon group velocity (magnitude)
+        alphas                                  :
+        tau_max                                 : Maximum correlation time to compute the ACF out to
+        tau_mean                                : Phonon's average lifetime
+        Lp                                      : Spacing
+        data                                    :
+        uncertainty                             :
+
+    :returns
+        This is to dump the output file, no return Obj.
+    """
+
+    paramkey = 'Parameters: v_g='+str(v_g)+' tau_mean='+str(tau_mean)+' Lp='+str(Lp)+' tau_max='+str(tau_max) +\
+               ' Kn='+str(v_g*tau_mean/Lp)+'\n'
+
     headkey = ("Time", "data[0]", "data[1]", "data[...]", "uncertainty[0]", "uncertainty[1]", "uncertainty[...]")
     separator = ' '
     with open(outfile, 'w') as writer:
@@ -29,113 +45,124 @@ def WriteDataFile(outfile, v_g, tau_mean, Lp, tau_max, alphas, data, uncertainty
                          separator+separator.join(map(str, uncertainty[:, i]))+"\n")
 
 
-# Model parameters
-
-tau_mean = 0.5              # Mean lifetime
-v_g = 5.0                   # Group velocity
-Lp = 200                    # Spacing between pores
-
+# Define initial parameters
+tau_mean = 0.5                          # Mean lifetime
+v_g = 5.0                               # Group velocity
+Lp = 200                                # Spacing between pores
 k_bulk = tau_mean * (v_g**2) / 3.0      # Bulk thermal conductivity
 
 # Simulation parameters
+tau_max = 25 * tau_mean                 # Maximum correlation time to compute the ACF out to
+time_intervals = 10001                  # Time intervals
+nblocks = 4                             # Number of blocks to average
+samplesPerBlock = 500                   # Number of samples in each block
 
-tau_max = 25 * tau_mean     # Maximum correlation time to compute the ACF out to
-time_intervals = 10001      # Time intervals
-nblocks = 4
-samplesPerBlock = 500
 
+# Define sweep parameter
+alphas = np.linspace(0.0, 1, 2)
 
-# sweep parameters
-alphas = np.linspace(0.0, 1, 21)
+# Correlation time
+tau = np.linspace(0.0, tau_max, time_intervals)
+Jn = 7
+J = np.asarray([[np.sign(tau[i] - Jn * tau_mean) for i in range(time_intervals)] for j in range(3)])      # Flux
 
-tau = np.linspace(0.0, tau_max, time_intervals)     # Lifetime
-J = np.asarray([[np.sign(tau[i] - 7 * tau_mean) for i in range(time_intervals)] for j in range(3)])
 tic = time.time()
+acf = ACF(tau, J)                       # Compute autocorrelation
+toc = time.time()
+
+print('timing = ' + str(toc - tic))
 print(np.shape(J), np.shape(tau))
+
+# Plot heat flux components along Cartesian axes
 plt.figure()
 plt.plot(tau, J[0, :])
 plt.plot(tau, J[1, :])
 plt.plot(tau, J[2, :])
-# plt.show()
-#
-# exit()
-c = ACF(tau, J)
-toc = time.time()
-print('timing = ' + str(toc - tic))
-quickmultiplot([[tau, c[i]] for i in range(3)])
+plt.show()
+
+# Plot autocorrelation components along Cartesian axes
+quickmultiplot([[tau, acf[i]] for i in range(3)])
+
 tic = time.time()
-cfft = ACF_FFT(tau, J)
-print(np.shape(cfft))
-# exit()
+acf_fft = ACF_FFT(tau, J)               # Compute autocorrelation using discrete fourier transform
 toc = time.time()
+
 print('timing = ' + str(toc - tic))
-quickmultiplot([[tau, cfft[i]] for i in range(3)])
-quickmultiplot([[tau, cfft[0]], [tau, c[0]]])
-c, I = getRandomPhononSpecWall(tau_max, time_intervals, v_g, tau_mean, Lp, 0)
 
-ACFm, ACFe, km, ke, ACFm_fft, ACFe_fft, km_fft, ke_fft = averagePhonons(nblocks, samplesPerBlock, tau_max, time_intervals, v_g, tau_mean, Lp, 0)
+# Plot autocorrelation components along Cartesian axes
+quickmultiplot([[tau, acf_fft[i]] for i in range(3)])
+
+# Compare autocorrelation components along x using the two previous methods
+quickmultiplot([[tau, acf_fft[0]], [tau, acf[0]]])
+
+tic = time.time()
+# Compute autocorrelation function and thermal conductivity for phonons flying between walls with spectral scattering
+acf, I = getRandomPhononSpecWall(tau_max, time_intervals, v_g, tau_mean, Lp, 0)
+
+# Compute average and standard deviation of autocorrelation and thermal conductivity
+ACFm, ACFe, km, ke = averagePhonons(nblocks, samplesPerBlock, tau_max, time_intervals,
+                                    v_g, tau_mean, Lp, 0, mode="SpecWall")
 toc = time.time()
+
 print('timing = '+str(toc - tic))
-
-quickmultiplot([[tau,ACFm[0]],[tau,ACFm_fft[0]]])
-
-quickmultierrplot([[tau,ACFm[i],ACFe[i]] for i in range(3)])
-quickmultierrplot([[tau,ACFm_fft[i],ACFe_fft[i]] for i in range(3)])
-quickmultierrplot([[tau,km[i],ke[i]] for i in range(3)])
-quickmultierrplot([[tau,km_fft[i],ke_fft[i]] for i in range(3)])
 print('kappa_bulk = '+str(k_bulk))
-print('kappa_xyz = '+str(km[:,-1]))
-print('kappa_xyz_fft = '+str(km_fft[:,-1]))
-
+print('kappa_xyz = '+str(km[:, -1]))
 print('timing = '+str(toc - tic))
+print(tau_mean*v_g**2/3.0)
+print(km[:, -1])
 
-print((tau_mean)*(v_g**2)/3.0)
-print(km[:,-1])
+# Plot correlation along x
+quickmultiplot([[tau, ACFm[0]]])
+# Plot mean and standard deviation of correlation along Cartesian axes
+quickmultierrplot([[tau, ACFm[i], ACFe[i]] for i in range(3)])
+# Plot mean and standard deviation of conductivity along Cartesian axes
+quickmultierrplot([[tau, km[i], ke[i]] for i in range(3)])
 
 
 # START SIMULATION: Perform a sweeping over alpha
 
-#import timeit
-#tic = timeit.timeit()
+# Define correlation time
 tau = np.linspace(0.0,tau_max,time_intervals)
 
+# Allocate memory to correlation and thermal conductivity blocks
 mACFtab = np.zeros([len(alphas), 3, time_intervals])
 eACFtab = np.zeros([len(alphas), 3, time_intervals])
-mktab   = np.zeros([len(alphas), 3, time_intervals])
-ektab   = np.zeros([len(alphas), 3, time_intervals])
+mktab = np.zeros([len(alphas), 3, time_intervals])
+ektab = np.zeros([len(alphas), 3, time_intervals])
+
 
 for alphindex in range(len(alphas)):
-   alpha = alphas[alphindex]
-   print('alpha: '+str(alpha))
+    alpha = alphas[alphindex]
+    print('alpha: '+str(alpha))          # Print sweep parameter
 
-   ACFm, ACFe, km, ke = averagePhonons(nblocks, samplesPerBlock, tau_max, time_intervals, tau_0_0, v_g, d, alpha)
-   quickmultierrplot([[tau,ACFm[i],ACFe[i]] for i in range(3)])
-   quickmultierrplot([[tau,km[i],ke[i]] for i in range(3)])
-   print('kappa_bulk = '+str((tau_0_0)*(v_g**2)/3.0))
-   print('kappa_xyz = '+str(km[:,-1]))
+    ACFm, ACFe, km, ke = averagePhonons(nblocks, samplesPerBlock, tau_max,
+                                        time_intervals, tau_mean, v_g, Lp,
+                                        alpha, mode="SpecWall")        # Compute correlation and thermal conductivity
 
-   mACFtab[alphindex] = ACFm;
-   eACFtab[alphindex] = ACFe;
-   mktab[alphindex]   = km;
-   ektab[alphindex]   = ke;
+    quickmultierrplot([[tau, ACFm[i], ACFe[i]] for i in range(3)])     # Plot correlation along axes
+    quickmultierrplot([[tau, km[i], ke[i]] for i in range(3)])         # Plot conductivity along axes
+    print('kappa_bulk = '+str(tau_mean*v_g**2/3.0))
+    print('kappa_xyz = '+str(km[:, -1]))
 
-#toc = timeit.timeit()
-#print('timeing = '+str(tic - toc))
-print('kappa_bulk = '+str((tau_0_0)*(v_g**2)/3.0))
-print('kappa_xyz(alpha='+str(alphas[0])+') = '+str(mktab[0,:,-1]))
-print('kappa_xyz(alpha='+str(alphas[-1])+') = '+str(mktab[-1,:,-1]))
-print('kappa_xyz = '+str(mktab[0,:,-1]))
+    mACFtab[alphindex] = ACFm                                          # Mean correlation
+    eACFtab[alphindex] = ACFe                                          # Standard deviation of correlation
+    mktab[alphindex] = km                                              # Mean thermal conductivity
+    ektab[alphindex] = ke                                              # Standard deviation of thermal conductivity
+
+print('kappa_bulk = '+str(tau_mean*v_g**2/3.0))
+print('kappa_xyz(alpha='+str(alphas[0])+') = '+str(mktab[0, :, -1]))
+print('kappa_xyz(alpha='+str(alphas[-1])+') = '+str(mktab[-1, :, -1]))
+print('kappa_xyz = '+str(mktab[0, :, -1]))
 
 
 # Save the data as a big file
 
 ReflectionTag = 'specular-wall'
-#ReflectionTag = 'diffuse-wall'
-#ReflectionTag = 'specular-cylinder'
-KNtag  = str(v_g*tau_0_0/d)
-dirtag = ('x','y','z')
+KNtag = str(v_g*tau_mean/Lp)
+dirtag = ('x', 'y', 'z')
+
+# Generate output file
 for i in range(3):
-   outfile = ReflectionTag+'-ACF-'+dirtag[i]+'-Kn'+KNtag+'.txt'
-   WriteDataFile(outfile, v_g, tau_0_0, d, tau_max, alphas, mACFtab[:,i], eACFtab[:,i])
-   outfile = ReflectionTag+'-kappa-'+dirtag[i]+'-Kn'+KNtag+'.txt'
-#    WriteDataFile(outfile, v_g, tau_0_0, d, tau_max, alphas, mktab[:,i], ektab[:,i])
+    outfile = ReflectionTag+'-ACF-'+dirtag[i]+'-Kn'+KNtag+'.txt'
+    WriteDataFile(outfile, v_g, tau_mean, Lp, tau_max, alphas, mACFtab[:, i], eACFtab[:, i], time_intervals)
+    outfile = ReflectionTag+'-kappa-'+dirtag[i]+'-Kn'+KNtag+'.txt'
