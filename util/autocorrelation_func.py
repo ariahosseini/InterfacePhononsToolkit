77c6d900c80ddfe26d6c118fb76dfd24a95575f7
Aria Hosseini <aria_h@mit.edu>
2022-04-12 09:01:59 -0700

    Update autocorrelation_func.py

--- util/autocorrelation_func.py
+++ util/autocorrelation_func.py
@@ -1,89 +1,72 @@
 """
-autocorrelation_func.py computes the autocorrelation (ACF) using different methods
-
-Author: S. Aria Hosseini
-Email: shoss008@ucr.edu
+A method to compute the auto-correlation function(acf) using different methods
 """
 
 import numpy as np
 
 
-def ACF(time, J):
+def acf(time: np.ndarray, flux: np.ndarray) -> np.ndarray:
 
     """
-
-    Compute the autocorrelation of the instantaneous heat flux vector
+    Compute the auto-correlation of the instantaneous heat flux vector
 
     :arg
-        time                                 : Correlation time lag
-        J                                    : Instantaneous heat flux vector, Numpy array of 3 by np.shape(time)
+        time: np.ndarray
+            Correlation time lag
+        flux: np.ndarray
+            Instantaneous heat flux vector
     :returns
-        acf                                  : Autocorrelation
-
+        auto_correlation: np.ndarray
+            : Auto-correlation
     """
 
     time_intervals = time.size
-    acf = np.array([[np.dot(J[j, :time_intervals - i], J[j, i:]) for i in range(time_intervals)] for j in range(3)])
-    acf *= (time[-1] - time[0]) / float(time_intervals)   # Autocorrelation
-    return acf
+    auto_correlation = np.array([[np.dot(flux[j, :time_intervals - i], flux[j, i:])
+                     for i in range(time_intervals)] for j in range(3)])
+    auto_correlation *= (time[-1] - time[0]) / float(time_intervals)
+
+    return auto_correlation
 
 
-def ACF_FFT(time, J):
+def acf_fft(time: np.ndarray, flux: np.ndarray) -> np.ndarray:
 
     """
-
-    Compute autocorrelation of the instantaneous flux vector using fast fourier transform.
-
+    Compute auto-correlation of the instantaneous flux vector using fast fourier transform.
     To get rid of the assumed periodicity in the ACF using this method, and the symmetry that one gets in the result,
     the length of the vector is doubled and fill the 2nd half with zeros. The first half of the result is taken.
 
     :arg
-        time                                 : Correlation time lag
-        J                                    : Instantaneous heat flux vector, Numpy array of 3 by np.shape(time)
+        time: np.ndarray
+            Correlation time lag
+        flux: np.ndarray
+            Instantaneous heat flux vector
     :returns
-        acf                                  : Autocorrelation
-
+        auto_correlation: np.ndarray
+            : Auto-correlation
     """
 
-    time_intervals = np.shape(J)[1]
-    acf = np.zeros([3, time_intervals * 2])   # Autocorrelation
+    time_intervals = np.shape(flux)[1]
+    acf = np.zeros([3, time_intervals * 2])  # Auto-correlation
 
-    dt = time[-1] / float(time_intervals)     # Timestep
+    dt = time[-1] / float(time_intervals)  # Time-step
     for j in range(3):
-        dft = np.fft.fft(np.concatenate([J[j], np.zeros(time_intervals)]))
+        dft = np.fft.fft(np.concatenate([flux[j], np.zeros(time_intervals)]))
         acf[j] = np.fft.ifft(dft * np.conjugate(dft)) * dt
     return acf[:, :time_intervals]
 
 
-def ACF_NP(J):
-
-    # Compute the autocorrelation of the instantaneous flux vector using numpy correlation command
-
-    acf = [np.correlate(J[i]) for i in range(3)]
-
-    return acf   # Autocorrelation
-
-
-def _ACF(time, J):
+def acf_numpy(flux: np.ndarray) -> np.ndarray:
 
     """
-
-    Compute the autocorrelation of the instantaneous heat flux vector.
-    Same opperation as ACF, but laid out differently
-
+    # Compute the auto-correlation of the instantaneous flux vector using numpy correlation command
     :arg
-        time                                 : Autocorrelation time lag
-        J                                    : Instantaneous heat flux vector, Numpy array of 3 by np.shape(time)
+        flux: np.ndarray
+            Instantaneous heat flux vector
     :returns
-        acf                                  : Autocorrelation
-
+        auto_correlation: np.ndarray
+            : Auto-correlation
     """
 
-    acf = 0 * J
-    time_intervals = time.size
-    for j in range(3):
-        for i in range(time_intervals):
-            acf[j, i] = np.dot(J[j, :time_intervals - i], J[j, i:])
-        acf *= (time[-1]-time[0])/float(time_intervals)
+    auto_correlation = np.array([np.correlate(flux[i]) for i in range(3)])
 
-    return acf
+    return auto_correlation
